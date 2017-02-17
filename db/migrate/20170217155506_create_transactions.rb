8e350d5634376e786657d4df33c3a7113eeacf33
Gregory Havenga <gregory.havenga@gmail.com>
2017-02-17 17:56:04 +0200

    Added controller and model

---
+++ db/migrate/20170217155506_create_transactions.rb
@@ -0,0 +1,15 @@
+class CreateTransactions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :transactions do |t|
+      t.integer :type
+      t.datetime :time
+      t.double :amount
+      t.string :account
+      t.string :client
+      t.integer :period
+      t.string :note
+
+      t.timestamps
+    end
+  end
+end
